d22a78eead1785b25e05ce502d7cdf36f711daf7
Mateus Celestino Delleteze <130002471+MateusCelestino@users.noreply.github.com>
2026-04-10 22:08:35 -0300

    login e senha

--- Login/components/Login.tsx
+++ Login/components/Login.tsx
@@ -1,55 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
-export default function Login() {
+
+export default function Login({navigation}: any) {
+  const [usuario, setUsuario] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
+  
+  function handleLogin() {
+    fetch ("https://localhost:7019/api/Login",
+     {
+      
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Email: usuario,
+        Senha: senha,
+      }),
+    })
+    .then((resposta) => { console.log(resposta) 
+    if (resposta.ok) {
+      console.log("Login bem-sucedido");
+      // navigation.navigate("Home");
+    } else {
+      console.log("Login falhou");
+    }
+
+    });
+      
+  }
   return (
     <View style={styles.container}>
-        <StatusBar style="auto" />
-        <View style={styles.titleContainer2}>
+      <StatusBar style="auto" />
+      <View style={styles.titleContainer2}>
         <Text style={styles.title2}>Login</Text>
-       
-      
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter your username" />
+
+
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Enter your username" onChangeText={setUsuario} />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Enter your password" onChangeText={setSenha} secureTextEntry />
+        </View>
+        <TouchableOpacity onPress={handleLogin} style={styles.titleContainer}>
+          <Text style={styles.title}>Entrar</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter your password" secureTextEntry={true} />
-      </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Entrar</Text>
-      </View>
-       </View>
     </View>
-  );        
-    
- 
+  );
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+
     flex: 1,
     backgroundColor: '#6C63FF',
-   
-    
+
+
 
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center', 
-    color: '#fff',   
+    textAlign: 'center',
+    color: '#fff',
   },
 
 
   inputContainer: {
     marginVertical: 10,
-        width: '80%',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        padding: 10,
-        backgroundColor: '#fff',
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: '#fff',
   },
   titleContainer: {
     marginBottom: 20,
@@ -60,16 +89,16 @@ const styles = StyleSheet.create({
   },
   titleContainer2: {
     flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: 24,
-        
-    
-},
-title2: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+
+
+  },
+  title2: {
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center', 
-    color: '#fff',   
-},
+    textAlign: 'center',
+    color: '#fff',
+  },
 });
